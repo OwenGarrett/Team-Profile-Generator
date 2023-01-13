a1ceaa141d2473a45278f0671d34766143b4a404
Owen Garrett <owengarrett@Owens-Air.lan>
2023-01-12 21:26:37 -0500

    built out inquirer prompts with manager, intern, engineer sections

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ inquirer
     },
     {
       type: "input",
-      message: "What is the employee ID?",
-      name: "employeeId",
+      message: "What is the manager employee ID number?",
+      name: "managerId",
     },
     {
       type: "input",
       message: "What is your manager's email address?",
-      name: "emailAddress",
+      name: "managerAddress",
     },
     {
       type: "input",
@@ -29,7 +29,7 @@ inquirer
     },
   ])
   .then((response) => {
-    const newManager = new Manager(response.managerName, response.employeeId, response.emailAddress, response.officeNumber);
+    const newManager = new Manager(response.managerName, response.managerId, response.managerAddress, response.officeNumber);
     myTeam.push(newManager)
     
     displayMenu()
@@ -37,11 +37,73 @@ inquirer
 
 
 function displayMenu() {
-    inquirer.prompt([
+    return inquirer.prompt([
 // insert "list" type prompt with name set to "choice" to give user choices "Add an Engineer", "Add an Intern", "Finish"
+type: 'list',
+name: 'menu',
+message: 'Please select an option to continue:',
+choices: ['Add Engineer', 'Add Intern', 'Finish']
     ])
     .then(response => {
         console.log(response);
         // create if/else-if statement for different choices
+        if 'Add Engineer' {
+          inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your engineer's name?",
+      name: "engineerName",
+    },
+    {
+      type: "input",
+      message: "What is the manager employee ID number?",
+      name: "engineerId",
+    },
+    {
+      type: "input",
+      message: "What is your manager's email address?",
+      name: "engineerAddress",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "engineerGithub",
+    },
+  ])
+  .then((response) => {
+    const newEngineer = new Engineer(response.engineerName, response.engineerId, response.engineerAddress, response.engineerGithub);
+    myTeam.push(newEngineer)
+        }
     })
-}
+
+    else if 'Add Intern' {
+          inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your intern's name?",
+      name: "internName",
+    },
+    {
+      type: "input",
+      message: "What is the intern ID number?",
+      name: "internId",
+    },
+    {
+      type: "input",
+      message: "What is your intern's email address?",
+      name: "internAddress",
+    },
+    {
+      type: "input",
+      message: "What is your school?",
+      name: "internSchool",
+    },
+  ])
+  .then((response) => {
+    const newIntern = new Intern(response.internName, response.internId, response.internAddress, response.internSchool);
+    myTeam.push(newIntern)
+        }
+    })
+};
